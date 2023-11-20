78b5781b228c140c4cd1627e1214be0e131a075d
FacuReartes <facureartes9@gmail.com>
2023-11-20 19:07:50 -0300

    a

--- MicroservicioAlquileres/trabajoPracticoIntegrador/src/main/java/com/bda/trabajoPracticoIntegrador/Service/Implementacion/AlquilerServiceImpl.java
+++ MicroservicioAlquileres/trabajoPracticoIntegrador/src/main/java/com/bda/trabajoPracticoIntegrador/Service/Implementacion/AlquilerServiceImpl.java
@@ -194,104 +194,41 @@ public class AlquilerServiceImpl implements AlquilerService {
         return duration.toMinutes();
     }
 
-    public Alquiler finalizarAlquiler(int id, String moneda) {
-        log.info("Iniciando finalizarAlquiler para el alquiler con ID() ");
+    public Alquiler finalizarAlquiler(AlquilerDto alquilerDto, String moneda) {
+        log.info("Iniciando finalizarAlquiler para el alquiler con ID() ", alquilerDto.getId());
         // Obtener la información del alquiler
-        Alquiler alquiler = getById(id);
-
-        EstacionDto estacionRetiro = estacionService.obtenerEstacionDto(alquiler.getEstacionRetiro());
-        EstacionDto estacionDevolucion = estacionService.obtenerEstacionDto(alquiler.getEstacionDevolucion());
+        Alquiler alquiler = getById(alquilerDto.getId());
         log.info("Alquiler: {}", alquiler);
         if (alquiler != null && alquiler.getFechaHoraDevolucion() == null) {
 
+            // Establecer la fecha y hora de devolución
             alquiler.setFechaHoraDevolucion(LocalDateTime.now());
 
             // Calcular la distancia en kilómetros entre las estaciones de retiro y devolución
-            double distanciaEnKm = estacionService.calcularDistancia(estacionRetiro.getLatitud(), estacionRetiro.getLongitud(),estacionDevolucion.getLatitud(), estacionDevolucion.getLongitud());
+            double distanciaEnKm = estacionService.calcularDistancia(alquilerDto.getEstacionRetiro().getLatitud(), alquilerDto.getEstacionRetiro().getLongitud(), alquilerDto.getEstacionDevolucion().getLatitud(), alquilerDto.getEstacionDevolucion().getLongitud());
             log.info("Distancia entre las estaciones: {} km", distanciaEnKm);
-
 
             // Calcular el monto total del alquiler
             double montoTotal = calcularMontoTotal(alquiler, distanciaEnKm);
             log.info("Monto total del alquiler: {}", montoTotal);
-            /*
-            // Aplicar descuento si es un día promocional
-            // esto esta en la funcion de monto
-            /*
-            if (esDiaPromocional()) {
-                double porcentajeDescuento = obtenerDescuentoPorDiaPromocional(alquiler.getFechaHoraRetiro());
-                montoTotal *= (1 - porcentajeDescuento);
-                log.info("Descuento aplicado: {}%", porcentajeDescuento * 100);
-            }
 
-
-             */
-
-            // Llamada al microservicio externo de cotizaciones
             String cotizacionApiUrl = "http://34.82.105.125:8080/convertir";
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
 
-            // Armar la solicitud
-            String requestBody = "{\"moneda_destino\":\"" + moneda + "\",\"importe\":" + montoTotal + "}";
+            // Obtener la cotización
+            double montoConvertido = exchangeService.obtenerCotizacion(cotizacionApiUrl, moneda, montoTotal);
 
-            // Configurar la solicitud HTTP
-            HttpEntity<String> entity = new HttpEntity<>(requestBody, headers);
+            // Calcular el monto final
+            double montoFinal = montoConvertido * montoTotal;
 
-            try {
-                // Log para registrar la solicitud al microservicio de cotizaciones
-                log.info("Realizando solicitud al microservicio de cotizaciones. URL: {}", cotizacionApiUrl);
+            alquiler.finalizar(montoFinal);
 
-                // Realizar la solicitud POST
-                ResponseEntity<String> response = restTemplate.exchange(cotizacionApiUrl, HttpMethod.POST, entity, String.class);
+            // Guardar la instancia actualizada en la base de datos
+            return update(alquiler.getId(), alquiler);
 
-                // Log para registrar la respuesta del microservicio
-                log.info("Respuesta del microservicio: {}", response);
-
-                log.info(response.getStatusCode().toString());
-
-                // Manejar la respuesta del microservicio de cotizaciones según sea necesario
-                if (response.getStatusCode().is2xxSuccessful()) {
-                    // Extraer y manejar la respuesta del microservicio
-                    String cotizacionResponse = response.getBody();
-
-                    // Log para registrar la respuesta detallada del microservicio
-                    log.info("Respuesta detallada del microservicio: {}", cotizacionResponse);
-
-                    // Parsear la respuesta del microservicio para obtener el monto convertido
-                    double montoConvertido = parsearRespuestaDelMicroservicio(cotizacionResponse, moneda);
-
-                    // el api ya hace este calculo
-                    // double montoFinal = montoConvertido * montoTotal;
-
-                    // Actualizar el monto en el objeto de alquiler
-                    alquiler.setMonto(montoConvertido);
-
-
-                    // Establece estado a finalizado
-                    alquiler.setEstado(2);
-
-                    // Guardar la instancia actualizada en la base de datos
-                    return update(alquiler.getId(), alquiler);
-                } else {
-                    // Log para registrar el fallo de la solicitud al microservicio de cotizaciones
-                    log.error("La solicitud al microservicio de cotizaciones falló. Código de estado: {}", response.getStatusCode());
-
-                    // Manejar el caso en que la solicitud al microservicio de cotizaciones falla
-                    throw new HttpClientErrorException(response.getStatusCode());
-                }
-            } catch (Exception e) {
-                // Log para registrar cualquier excepción ocurrida durante la ejecución del bloque try
-                log.error("Error al realizar la solicitud al microservicio de cotizaciones", e);
-
-                // Puedes manejar la excepción según tus necesidades
-                throw e;
-            }
         } else {
             log.error("Alquiler no encontrado o ya devuelto");
             throw new NoSuchElementException("Alquiler no encontrado o ya devuelto");
         }
-    }
 
 
     private double parsearRespuestaDelMicroservicio(String cotizacionResponse, String monedaEsperada) {
