98d089117be95bfaf985a7bcbfaf1e9266a56f64
Ulises Gutierrez <89874@sistemas.frc.utn.edu.ar>
2024-12-21 14:25:03 -0300

    Agrupación del proyecto de Backend

--- MicroservicioEstaciones/trabajoPracticoIntegrador/src/main/java/com/bda/trabajoPracticoIntegrador/Estaciones/trabajoPracticoIntegrador/Repository/EstacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bda.trabajoPracticoIntegrador.Estaciones.trabajoPracticoIntegrador.Repository;
-
-import com.bda.trabajoPracticoIntegrador.Estaciones.trabajoPracticoIntegrador.Entity.Estacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstacionRepository extends JpaRepository<Estacion, Integer> {
-}
